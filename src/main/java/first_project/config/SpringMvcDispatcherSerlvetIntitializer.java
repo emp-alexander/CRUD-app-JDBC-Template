0671f74ccbef74bbd25cea37d7e3de32c14b0509
admin <borisov2261@gmail.com>
2024-01-28 14:04:51 +0700

    Настроил конфиг

---
+++ src/main/java/first_project/config/SpringMvcDispatcherSerlvetIntitializer.java
@@ -0,0 +1,26 @@
+package first_project.config;
+
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class SpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
